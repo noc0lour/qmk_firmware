5162f6ca28ab6c0a9cd7392378e2d8705d0c31b7
Andrej Rode <mail@andrejro.de>
2023-12-25 17:13:19 +0100

    noc0lour: updates & fixes

--- keyboards/idobao/id75/keymaps/noc0lour/keymap.c
+++ keyboards/idobao/id75/keymaps/noc0lour/keymap.c
@@ -23,15 +23,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* QWERTZ (German for Neo2 mapping in OS)
  * .--------------------------------------------------------------------------------------------------------------------------------------.
- * | ESC    | 1      | 2      | 3      | 4      | 5      |        |        | 6      | 7      | 8      | 9      | 0      | ß      |        |
+ * | ESC    | 1      | 2      | 3      | 4      | 5      |        |        | 6      | 7      | 8      | 9      | 0      | ß      | =      |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
  * | TAB    | Q      | W      | E      | R      | T      | DEL    | BACKSP | Z      | U      | I      | O      | P      | '      | +      |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
  * | CAP LK | A      | S      | D      | F      | G      |        | ENTER  | H      | J      | K      | L      | Ö      | Ä      | #      |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
- * | LSHIFT | <      | Y      | X      | C      | V      | B      | END    | N      | M      | ,      | .      | -      | RSHIFT | \
+ * | LSHIFT | <      | Y      | X      | C      | V      | B      | END    | N      | M      | ,      | .      | -      | RSHIFT | \      |
  * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+--------+-----------------+--------+--------|
- * | LCTRL  | LALT   | LGUI   | FN     | SPACE  | SPACE  |        |        | SPACE  | SPACE  | FN     | RALT   | RGUI   | RCTRL  |        |
+ * | LCTRL  | LALT   | LGUI   | FN     | SPACE  | SPACE  |        |        | SPACE  | SPACE  | FN     | RALT   | RGUI   | RCTRL  | UP     |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
 
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_DEL,  KC_BSPC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, \
     KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_HOME, KC_ENTER,KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, \
     KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_END,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_BSLS, \
-    KC_LCTL, KC_LALT, KC_LGUI, MO(_FN), KC_SPC,  KC_SPC,  KC_PGUP ,KC_PGDN, KC_SPC,  KC_SPC,  MO(_FN), KC_RALT, KC_RGUI, KC_RCTL, KC_NO \
+    KC_LCTL, KC_LALT, KC_LGUI, MO(_FN), KC_SPC,  KC_SPC,  KC_PGUP ,KC_PGDN, KC_SPC,  KC_SPC,  MO(_FN), KC_RALT, KC_RGUI, KC_RCTL, KC_UP \
  ),
 
 /* FUNCTION
